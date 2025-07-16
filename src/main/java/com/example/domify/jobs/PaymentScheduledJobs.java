139ff7e5d9d76b8c1be592ce0f7d73b105bcee01
stefannovkovski <stefannovkovski10@gmail.com>
2025-07-17 00:12:36 +0200

    added scheduler

---
+++ src/main/java/com/example/domify/jobs/PaymentScheduledJobs.java
@@ -0,0 +1,22 @@
+package com.example.domify.jobs;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentScheduledJobs {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Scheduled(cron = "0 5 0 16-17 * *")
+    @Transactional
+    public void callReminderProcedure() {
+        entityManager
+                .createNativeQuery("CALL domify.send_monthly_payment_reminders()")
+                .executeUpdate();
+    }
+}
